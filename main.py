0e1df7fb67b0b53372a3a3674e5a903d4c3a2e58
LuSP19 <48345486+LuSP19@users.noreply.github.com>
2021-11-09 02:02:40 +0500

    Улучшения кода после проверки

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 from pathlib import Path
 from time import sleep
 from urllib.parse import unquote, urlparse
@@ -10,24 +11,28 @@ from dotenv import load_dotenv
 
 def extract_ext(url):
     path = unquote(urlparse(url).path)
-    filename = os.path.split(path)[1]
-    return os.path.splitext(filename)[1]
+    return os.path.splitext(path)[1]
 
 
-def fetch_spacex_last_launch(directory):
+def get_image(directory, filename, image_link, params=None):
+    file_path = Path(directory)/filename
+    response = requests.get(image_link, params=params)
+    response.raise_for_status()
+
+    with file_path.open('wb') as file:
+        file.write(response.content)
+
+
+def fetch_spacex_launch_images(directory, launch_number):
     url = 'https://api.spacexdata.com/v4/launches/'
     response = requests.get(url)
-    space_x_resp = response.json()[12]
+    response.raise_for_status()
+    space_x_resp = response.json()[launch_number]
     image_links = space_x_resp['links']['flickr']['original']
     
     for image_number, image_link in enumerate(image_links, start=1):
-        filename = 'spacex{0}{1}'.format(str(image_number), extract_ext(image_link))
-        file_path = Path(directory)/filename
-        response = requests.get(image_link)
-        response.raise_for_status
-    
-        with file_path.open('wb') as file:
-            file.write(response.content)
+        filename = 'spacex{0}{1}'.format(image_number, extract_ext(image_link))
+        get_image(directory, filename, image_link)
 
 
 def fetch_nasa_images(directory, count, nasa_api_key):
@@ -35,41 +40,31 @@ def fetch_nasa_images(directory, count, nasa_api_key):
     params = {'api_key':nasa_api_key, 'count':count}
     nasa_images = []
     response = requests.get(url, params=params)
+    response.raise_for_status()
 
     for item in response.json():
         nasa_images.append(item['hdurl'])
     
     for image_number, image_link in enumerate(nasa_images, start=1):
-        filename = 'nasa{0}{1}'.format(str(image_number), extract_ext(image_link))
-        file_path = Path(directory)/filename
-        response = requests.get(image_link)
-        response.raise_for_status
-    
-        with file_path.open('wb') as file:
-            file.write(response.content)
+        filename = 'nasa{0}{1}'.format(image_number, extract_ext(image_link))
+        get_image(directory, filename, image_link)
 
 
 def fetch_nasa_epic_images(directory, nasa_api_key):
     url = 'https://api.nasa.gov/EPIC/api/natural/images'
-    img_url_template = 'https://api.nasa.gov/EPIC/archive/natural/{0}/{1}/{2}/png/{3}.png'
+    img_url_template = 'https://api.nasa.gov/EPIC/archive/natural/{0}/png/{1}.png'
     params = {'api_key':nasa_api_key}
     response = requests.get(url, params=params)
+    response.raise_for_status()
 
     for item in response.json():
         image_id = item['image']
-        year_month_day = item['image'].split('_')[2][:8]
-        year = year_month_day[:4]
-        month = year_month_day[4:6]
-        day = year_month_day[6:]
-
-        image_url = img_url_template.format(year, month, day, image_id)
+        image_timestamp = image_id.split('_')[-1]
+        image_datetime = datetime.strptime(f'{image_timestamp}', '%Y%m%d%H%M%S')
+        image_timestamp_path = image_datetime.strftime('%Y/%m/%d')
+        image_link = img_url_template.format(image_timestamp_path, image_id)
         filename = '{0}.png'.format(image_id)
-        file_path = Path(directory)/filename
-        response = requests.get(image_url, params=params)
-        response.raise_for_status
-
-        with file_path.open('wb') as file:
-            file.write(response.content)
+        get_image(directory, filename, image_link, params=params)
 
 
 def main():
@@ -77,31 +72,27 @@ def main():
     nasa_api_key = os.getenv('NASA_API_KEY')
     tg_bot_token = os.getenv('TG_BOT_TOKEN')
     chat_id = os.getenv('CHAT_ID')
-    delay = os.getenv('SPACE_IMAGES_SEND_DELAY')
-    if delay:
-        delay = int(delay)
-    else:
-        delay = 86400
+    delay = int(os.getenv('SPACE_IMAGES_SEND_DELAY', '86400'))
     directory = 'images'
     nasa_images_count = 10
+    spacex_launch_number = 12
 
     Path(directory).mkdir(parents=False, exist_ok=True)
 
-    fetch_spacex_last_launch(directory)
+    fetch_spacex_launch_images(directory, spacex_launch_number)
     fetch_nasa_images(directory, nasa_images_count, nasa_api_key)
     fetch_nasa_epic_images(directory, nasa_api_key)
 
     bot = telegram.Bot(token=tg_bot_token)
     space_images = os.listdir(directory)
 
-    while True:
-        if space_images:
-            file_path = os.path.join(directory, space_images.pop())
-            bot.send_photo(chat_id=chat_id, photo=open(file_path, 'rb'))
-            sleep(delay)
-        else:
-            print('All images have been sent')
-            break
+    while space_images:
+        file_path = os.path.join(directory, space_images.pop())
+        with open(file_path, 'rb') as photo:
+            bot.send_photo(chat_id=chat_id, photo=photo)
+        sleep(delay)
+
+    print('All images have been sent')
 
 
 if __name__ == '__main__':
